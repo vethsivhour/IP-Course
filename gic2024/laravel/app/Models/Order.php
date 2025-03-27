72726bf03f580b44c61d5d839c619192c6ba1435
sivhour <vethsivhour@gmail.com>
2025-03-27 09:59:24 +0700

    Task 3: Define the following "Mutator" and "Accessor"

--- gic2024/laravel/app/Models/Order.php
+++ gic2024/laravel/app/Models/Order.php
@@ -3,10 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 
 class Order extends Model
 {
     protected $fillable = ['order_date', 'total_price', 'customer_id'];
+
+    protected function orderDate(): Attribute
+    {
+        return Attribute::make(
+            // Mutator: Convert input format to MySQL format before saving
+            set: fn ($value) => Carbon::createFromFormat('d/m/Y H:i:s', $value)->format('Y-m-d H:i:s'),
+            
+            // Accessor: Convert database format to user format when retrieving
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y H:i:s')
+        );
+    }
 
     public function payments()
     {
